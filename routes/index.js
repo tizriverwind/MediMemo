1d5cabc3d8ccd20af21fd47a9f734cf434dd0f89
Vickidiaz <vickidiaz42@gmail.com>
2023-11-03 20:25:40 -0700

    testing changes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ let router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   // Send a response without rendering a view
-  res.send("Welcome to MediMemo!");
+  //res.send("Welcome to MediMemo!");
+  res.json([1, 2, 3]);
 });
 
 export default router;
